abb2ae25dbec649b7809dcc1a42098135f90c468
Hoang Anh <anh.truong@wiki-solution.com>
2023-11-20 11:34:52 +0700

    Add new project SwiftUIKnowledge

---
+++ SwiftUIKnowledge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIKnowledge
+//
+//  Created by Hoang Anh on 20/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIKnowledge/SwiftUIKnowledgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKnowledgeApp.swift
+//  SwiftUIKnowledge
+//
+//  Created by Hoang Anh on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKnowledgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
